e0f3e2c4aa042a032e37e891deae7e5fd7c2fc87
codesomehax <markiewicz.darek.dm@gmail.com>
2022-06-20 11:12:29 +0200

    Replaced code in place

--- src/mpk/Region.java
+++ src/mpk/Region.java
@@ -4,20 +4,12 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class Region {
+    public Set<Employee> employees;
+    public Set<Route> routes;
+
     private int id;
     private String name;
     private Manager manager;
-
-    public Manager getManager() {
-        return manager;
-    }
-
-    public void setManager(Manager manager) {
-        this.manager = manager;
-    }
-
-    public Set<Employee> employees;
-    public Set<Route> routes;
 
     public Region(int id, String name, Manager manager) {
         this.id = id;
@@ -26,6 +18,14 @@ public class Region {
 
         employees = new HashSet<Employee>();
         routes = new HashSet<Route>();
+    }
+
+    public Manager getManager() {
+        return manager;
+    }
+
+    public void setManager(Manager manager) {
+        this.manager = manager;
     }
 
     public int getId() {
